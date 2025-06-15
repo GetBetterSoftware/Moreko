bcca273a12eacd1f21f6a599bfe663d0485a3f6a
Osmond-Matsela <osmondmatsela@gmail.com>
2025-06-15 22:59:43 +0200

    images

---
+++ src/app/components/about.tsx
@@ -0,0 +1,11 @@
+import { colors } from './colors';
+
+
+export default function About() {
+    return (
+        <div className={`bg-[#F9F9F9] w-full h-140 flex flex-row justify-center items-center gap-30`}>
+            <div className={`bg-[#B12B24] w-70 h-100`}></div>
+            <div className={`bg-[#222222] w-200 h-100`}></div>
+        </div>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import HeroSection from "./components/heroSection";
+import About from "./components/about";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-        <HeroSection />
+        {/* <HeroSection /> */}
+        <About/>
     </div>
   );
 }
