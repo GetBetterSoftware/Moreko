e88df97f8380c84846c041cd1a51a1baea0a95d6
mkhabelemark <mimkhabjhb024@student.wethinkcode.co.za>
2025-06-15 23:10:56 +0200

    added map and form section that are both returned in the contact section

---
+++ src/app/components/contactForm.tsx
@@ -0,0 +1,22 @@
+export default function ContactForm(){
+    return (
+        <form>
+            <label> Full Name:</label>
+            <input></input>
+
+            <label> Email Address:</label>
+            <input></input>
+
+            <label> Subject:</label>
+            <input></input>
+
+            <label> Message:</label>
+            <input></input>
+
+            <input type="submit" value="Send"></input>
+        
+        </form>
+    )
+}
+
+
