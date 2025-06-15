35fae712073850a3546865b344f0046881b9d59f
Osmond-Matsela <osmondmatsela@gmail.com>
2025-06-15 21:43:59 +0200

    colors fixed

--- src/app/components/color.tsx
+++ src/app/components/color.tsx
@@ -1,7 +1,7 @@
 export const colors = {
-    Chewing_gum: "F6D6D4",
-    burgundy:"B12B24",
-    raisin_black:"222222",
-    white_smoke:"F9F9F9",
+    CHEWINGGUM: "#F6D6D4",
+    BURGUNDY:"#B12B24",
+    RAISINBLACK:"#222222",
+    WHITESMOKE:"#F9F9F9",
 }
 
