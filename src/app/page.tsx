c902331879c2ebeba1a94962ca7071463149e567
SizoDevelops <fierceincii@gmail.com>
2025-06-16 00:16:30 +0200

    Looks weird Im scrapping this idea

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import About from "./components/about";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center min-h-screen py-2">
+    <div className="flex flex-col items-center justify-start min-h-screen py-2">
         <HeroSection />
         {/* <About/> */}
     </div>
