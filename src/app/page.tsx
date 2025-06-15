7c4a4eb227304734e435b475390cfb56cc11423b
mkhabelemark <mimkhabjhb024@student.wethinkcode.co.za>
2025-06-16 01:27:52 +0200

    added form and embede map

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import HeroSection from "./components/heroSection";
+import ContactForm from "./components/contactForm";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
         <HeroSection />
+        <ContactForm/>
     </div>
   );
 }
