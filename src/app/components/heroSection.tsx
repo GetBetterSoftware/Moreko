f83cd0e2b12c44ea0efd8b4f74b6a2adc4f515a6
emmaloajhb024 <emmaloajhb024@student.wethinkcode.co.za>
2025-06-15 23:09:38 +0200

    Pushed

--- src/app/components/heroSection.tsx
+++ src/app/components/heroSection.tsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 export default function HeroSection() {
   return (
-    <div>
-        <h1>Hero Section</h1>
+    <div className={`bg-[#FF6363] h-screen max-h-[900px] max-w-[1440px]  w-screen p-10 m-0 flex flex-col items-center justify-center`}>
+        <div className='h-[700px] w-[80%] bg-[#ffffff] rounded'>
+            <h1>Hero Section</h1>
+        </div>
     </div>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
-import Image from "next/image";
 import HeroSection from "./components/heroSection";
 import About from "./components/about";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-        {/* <HeroSection /> */}
-        <About/>
+    <div className="flex flex-col items-center min-h-screen py-2">
+        <HeroSection />
+        {/* <About/> */}
     </div>
   );
 }
