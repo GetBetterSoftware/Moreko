440f945783db816e7562d88da5946b1983073d59
mkhabelemark <mimkhabjhb024@student.wethinkcode.co.za>
2025-06-16 01:40:02 +0200

    added pages components

---
+++ src/app/components/contacts.tsx
@@ -0,0 +1,12 @@
+import MapSection from "./map";
+import ContactForm from "./contactForm";
+
+export default function ContactPage(){
+    return (<section>
+        <h1> Contact Page</h1>
+         <div><ContactForm/></div>
+        <div><MapSection/></div>
+       
+        
+    </section>)
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import HeroSection from "./components/heroSection";
-import ContactForm from "./components/contactForm";
+import ContactPage from "./components/contacts";
 import About from "./components/about";
+
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-start min-h-screen py-2">
         <HeroSection />
+        
+        <About/>
+        <ContactPage/>
     </div>
   );
 }
