6da5ea2bbdad5f100986b8bb76f4b46af6662913
mkhabelemark <mimkhabjhb024@student.wethinkcode.co.za>
2025-06-16 02:05:21 +0200

    styled the contact page

--- src/app/components/contacts.tsx
+++ src/app/components/contacts.tsx
@@ -3,9 +3,9 @@ import ContactForm from "./contactForm";
 
 export default function ContactPage(){
     return (<section>
-        <h1> Contact Page</h1>
-         <div><ContactForm/></div>
-        <div><MapSection/></div>
+        <h1 className={`text-5xl font-bold flex justify-center mt-5`}> Contact Page</h1>
+         <div className="mt-5"><ContactForm/></div>
+        <div className="mt-5"><MapSection/></div>
        
         
     </section>)
