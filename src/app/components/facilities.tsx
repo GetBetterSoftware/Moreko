41c871fa72d68ca35e3841d48d739c9238d582bf
Osmond-Matsela <osmondmatsela@gmail.com>
2025-06-16 02:37:11 +0200

    Facilities

---
+++ src/app/components/facilities.tsx
@@ -0,0 +1,21 @@
+export default function Facilities() {
+    return (
+        <div className="bg-[#F9F9F9] max-w-[1440px] max-h-[900px] w-screen h-screen flex flex-col justify-center items-center">
+            <h2 className="text-5xl font-bold">Facilities</h2>
+            <div className="bg-[#F9F9F9] mt-5 w-300 h-160 flex flex-row justify-center items-center gap-10">
+                <div className="bg-[#B12B24] w-80 h-100 flex flex-col justify-center items-center">
+                    <h3>Dynamic charriculum</h3>
+                    <p></p>
+                </div>
+                <div className="bg-[#B12B24] w-80 h-100 flex flex-col justify-center items-center">
+                    <h3>Extra charricular activities</h3>
+                </div>
+                <div className="bg-[#B12B24] w-80 h-100 flex flex-col justify-center items-center">
+                    <h3>Community engagement</h3>
+                </div>
+            </div>
+            
+            
+        </div>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,15 @@
 import HeroSection from "./components/heroSection";
 import ContactPage from "./components/contacts";
 import About from "./components/about";
+import Facilities from "./components/facilities";
 
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-start min-h-screen py-2">
         <HeroSection />
-        
         <About/>
+        <Facilities/>
         <ContactPage/>
     </div>
   );
