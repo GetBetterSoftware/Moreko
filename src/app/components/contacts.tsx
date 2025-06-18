313b4b3af41b326530e7dfddbdbac73e2652d615
Osmond Matsela <osmatsejhb024@student.wethinkcode.co.za>
2025-06-18 13:02:01 +0200

    color change

--- src/app/components/contacts.tsx
+++ src/app/components/contacts.tsx
@@ -2,7 +2,7 @@ import MapSection from "./map";
 import ContactForm from "./contactForm";
 
 export default function ContactPage(){
-    return (<section>
+    return (<section >
         <h1 className={`text-5xl font-bold flex justify-center mt-5`}> Contact Page</h1>
          <div className="mt-5"><ContactForm/></div>
         <div className="mt-5"><MapSection/></div>
--- src/app/components/facilities.tsx
+++ src/app/components/facilities.tsx
@@ -1,21 +1,24 @@
 export default function Facilities() {
     return (
-        <div className="bg-[#F9F9F9] max-w-[1440px] max-h-[900px] w-screen h-screen flex flex-col justify-center items-center">
+        <div className="bg-[#F04E55] max-w-[1440px] max-h-[900px] w-screen h-screen flex flex-col justify-center items-center">
             <h2 className="text-5xl font-bold">Facilities</h2>
             <div className="bg-[#F9F9F9] mt-5 w-300 h-160 flex flex-row justify-center items-center gap-10">
-                <div className="bg-[#B12B24] w-80 h-100 flex flex-col justify-center items-center">
-                    <h3>Dynamic charriculum</h3>
-                    <p></p>
+                <div className="bg-[#F9F9F9] w-80 h-100 flex flex-col justify-center items-center">
+                    <div className="bg-[#B12B24] rounded-full w-35 h-35 "></div>
+                    <h3 className="mt-5">Dynamic charriculum</h3>
+                    <p className="m-4">Moreko thrives on more then just accademics, we want the best for our learners and ensure that we archieve this through sports like, Soccer, Netball, Volley ball, athletics and chess. We also have a school choir as well.</p>
                 </div>
-                <div className="bg-[#B12B24] w-80 h-100 flex flex-col justify-center items-center">
-                    <h3>Extra charricular activities</h3>
+                <div className="bg-[#F9F9F9] w-80 h-100 flex flex-col justify-center items-center">
+                    <div className="bg-[#B12B24] rounded-full w-35 h-35 "></div>
+                    <h3 className="mt-5">Extra charricular activities</h3>
+                    <p className="m-4">Moreko thrives on more then just accademics, we want the best for our learners and ensure that we archieve this through sports like, Soccer, Netball, Volley ball, athletics and chess. We also have a school choir as well.</p>
                 </div>
-                <div className="bg-[#B12B24] w-80 h-100 flex flex-col justify-center items-center">
-                    <h3>Community engagement</h3>
+                <div className="bg-[#F9F9F9] w-80 h-100 flex flex-col justify-center items-center">
+                    <div className="bg-[#B12B24] rounded-full w-35 h-35 "></div>
+                    <h3 className="mt-5">Community engagement</h3>
+                    <p className="m-4">Moreko thrives on more then just accademics, we want the best for our learners and ensure that we archieve this through sports like, Soccer, Netball, Volley ball, athletics and chess. We also have a school choir as well.</p>
                 </div>
             </div>
-            
-            
         </div>
     )
 }
